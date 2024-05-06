0f7e69e5a32476fafe4cdc6dc93792ba46d2927a
sanshray12 <sanshray12@gmail.com>
2024-05-06 19:13:05 +0530

    Refactoration of App.js code

--- frontendr1/src/App.js
+++ frontendr1/src/App.js
@@ -9,16 +9,6 @@ function App() {
     
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
 
         <Routes>
           <Route path="/login" element={<LoginPage />} />
